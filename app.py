9f4622bdb76f6eb51c6225a05d07820771c973a8
Sebastien St Vil <kseb0@hotmail.com>
2022-04-30 14:56:41 -0400

    .

---
+++ app.py
@@ -0,0 +1,8 @@
+from mastermindweb import app
+
+#######################################
+########### Run Website ##############
+#####################################
+
+if __name__ == '__main__':
+    app.run(debug=True)
