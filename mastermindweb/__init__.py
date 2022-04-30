6b7b101ba599697865db42dd4dce7d162d8d82ed
Sebastien St Vil <kseb0@hotmail.com>
2022-04-30 15:15:14 -0400

    register blueprints

--- mastermindweb/__init__.py
+++ mastermindweb/__init__.py
@@ -20,3 +20,24 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Migrate(app,db)
 
+#########################
+# LOGIN CONFIGS
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+
+
+##################################################
+
+
+from mastermindweb.core.views import core
+from mastermindweb.users.views import users
+from mastermindweb.error_pages.handlers import error_pages
+from mastermindweb.blog_posts.views import blog_posts
+
+app.register_blueprint(users)
+app.register_blueprint(blog_posts)
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
