62db6c14a02b075c670febaa46c1e0ec4922a55a
Sebastien St Vil <kseb0@hotmail.com>
2022-04-30 18:57:59 -0400

    add mastermind to welcome screen

--- mastermindweb/core/views.py
+++ mastermindweb/core/views.py
@@ -6,11 +6,17 @@ core = Blueprint('core',__name__)
 @core.route('/')
 def index():
 
-    page = request.args.get('page', 1, type=int)
-    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=10)
-    return render_template('index.html',blog_posts=blog_posts)
+    return render_template('index.html')
 
 @core.route('/info')
 def info():
 
     return render_template('info.html')
+
+
+@core.route('/blog')
+def blog():
+
+    page = request.args.get('page', 1, type=int)
+    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=10)
+    return render_template('viewblogposts.html',blog_posts=blog_posts)
